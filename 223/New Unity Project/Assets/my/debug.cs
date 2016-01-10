ed244ada2f90d3e979bb328286d39c5703b1effc
gmfnasg <gpx_mark_farng@hotamil.com>
2016-01-10 20:15:15 +0800

    進度

---
+++ 223/New Unity Project/Assets/my/debug.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class debug : MonoBehaviour {
+
+    public Text logtext;
+    public Camera camera;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (logtext != null && camera != null)
+        {
+            logtext.text = camera.gameObject.transform.position.ToString();
+        }
+	}
+}
